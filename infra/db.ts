648d7aa1081aac00c99eb8c4472fbaadf60f71d0
eduardossoares <eduardo21.net@gmail.com>
2026-06-15 21:05:27 -0300

    chore: turn default connectionString model on detabase connections

--- infra/db.ts
+++ infra/db.ts
@@ -3,12 +3,7 @@ import { Client, type QueryConfig } from "pg";
 export class pgDatabase {
   client = new Client({
     application_name: "prato-feito-backend",
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.POSTGRES_SSLMODE === "verify-full",
+    connectionString: process.env.DATABASE_URL,
   });
 
   async query(queryObject: QueryConfig | string) {
