fc0b672b3ea2b087e8450376b96aa529a8c89d7d
eduardossoares <eduardo21.net@gmail.com>
2026-07-21 19:01:38 -0300

    ci: use biome:write

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import { buildApp } from "./app";
 
 const PORT = Number(process.env.PORT || 8080);
 const HOST = process.env.HOST || "0.0.0.0";
-const FRONTEND_URL = process.env.FRONTEND_URL || ""
+const FRONTEND_URL = process.env.FRONTEND_URL || "";
 
 const app = buildApp();
 await app.register(fastifyCors, {
-  origin: [FRONTEND_URL]
-})
+  origin: [FRONTEND_URL],
+});
 
 try {
   app.listen({ port: PORT, host: HOST }, () => {
