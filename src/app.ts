70681ad4b68a61da9ddc273ed8f5225d437a324f
eduardossoares <eduardo21.net@gmail.com>
2026-08-06 19:23:24 -0300

    feat: add fastify swagger and fastify-type-provider-zod

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import Fastify from "fastify";
 import v1Routes from "./routes/v1.routes";
 import { errorHandler, notFoundHandler } from "./shared/http/handlers";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
 
 export function buildApp() {
   const app = Fastify({
     logger: true,
   });
+
+  app.setValidatorCompiler(validatorCompiler);
+  app.setSerializerCompiler(serializerCompiler);
 
   app.setErrorHandler(errorHandler);
   app.setNotFoundHandler(notFoundHandler);
